839ba35fe343f21fa465b82c24dff81139342756
Sebastiano Calella <sebastianocalella97@gmail.com>
2022-12-22 10:15:53 +0100

    fixed system to show numbers

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ createApp({
         getNumber(){
             axios.get('https://flynn.boolean.careers/exercises/api/random/int')
             .then((response) =>{
-                if (response%2 == 0){
+                if (response.data.response%2 == 0){
                     this.evenNumbers.push(response.data.response)
                 } else{
                     this.oddNumbers.push(response.data.response)
