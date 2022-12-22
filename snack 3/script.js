5ed1f5aca474a4a99f449bb16b9eb84c5519ead3
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-22 15:43:14 +0100

    implement user message on enter

--- snack 3/script.js
+++ snack 3/script.js
@@ -10,8 +10,18 @@ createApp({
         getAnswer(){
             axios.get('https://flynn.boolean.careers/exercises/api/random/sentence')
             .then((response) => {
-                this.chat.push(response.data.response)
+                this.chat.push({message : response.data.response, status : 'recived'})
             })
+        },
+        writeMessage(){
+            this.chat.push({message : this.userMessage, status : 'sent'})
+            this.userMessage = ''
+        },
+        chatting(){
+            this.writeMessage()
+            if(this.writeMessage!=''){
+                this.getAnswer();
+            }
         }
     },
     created(){
