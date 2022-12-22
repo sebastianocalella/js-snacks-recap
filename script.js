9047d425cc1fa04891a7d385d122c00435f67a1a
Sebastiano Calella <sebastianocalella97@gmail.com>
2022-12-22 10:05:17 +0100

    function logic structure

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
     return {
-        emailList : [],
+        odd : [],
+        even : []
     }
     },
     methods:{
-
+        getNumber(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+            .then((response) =>{
+                if (response%2 == 0){
+                    this.even.push(response.data)
+                } else{
+                    this.odd.push(response.data)
+                }
+            })
+        }
     },
     created(){
 
