f89b7bc32ba34bf2b19afe183d2fbc14057e4534
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-22 16:44:05 +0100

    try to assign array to data

--- snack 4/script.js
+++ snack 4/script.js
@@ -3,17 +3,28 @@ const { createApp } = Vue
 createApp({
     data() {
     return {
-    
+        numberList : ''
     }
     },
     methods:{
         getArrayNumbers(min,max,items){
             axios.get(`https://flynn.boolean.careers/exercises/api/array/integers?min=${min}&max=${max}&items=${items}`)
             .then((response) =>{
-                this.numberList = response
+                console.log(response.data.response)
+                this.numberList = response.data.response
                 console.log(this.numberList)
             })
+        },
+        sum(){
+            let sum = 0
+            for(let i=0; i<this.numberList.length; i++){
+                sum += this.numberlist[i]
+            }
+        },
+        cLog(element){
+            console.log(element)
         }
+
     },
     created(){
 
